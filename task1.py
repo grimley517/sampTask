3873e0c6df2dbc53a7112d085d7d84a832245b64
Brian Jones <ba.jones@me.com>
2014-06-04 16:10:20 +0100

    Create task1.py
    added task descrption

---
+++ task1.py
@@ -0,0 +1,9 @@
+'''
+The task here is to create a simple currency converter.
+
+Exchange rates should be able to be changed regularly by the user.
+
+The user should be able to enter an ammount, select the chosen currency, and the currency into which this should be converted
+
+the ficure shown should be correct to two decimal places.
+'''
