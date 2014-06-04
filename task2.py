8a38a808ae5d8ab7246119a25ae5cd9776d5267b
Brian Jones <ba.jones@me.com>
2014-06-04 16:14:28 +0100

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,12 @@
+'''
+Address Book
+
+a simple address book holds names addresses and telephone numbers for friends and family.
+
+the system stores a surname and a name, two lines of the address and the postcode, a telephone number, date of birth, email address.
+
+this program will be able to search the data file by surname to retrieve and display the details for a contact.
+
+by date of birth to retrieve and display all contacts with a birthday in a particular month.
+
+'''
