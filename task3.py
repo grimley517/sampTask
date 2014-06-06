657a70ec8193dd46cb953b74f02d99adee174d5b
Brian Jones <ba.jones@me.com>
2014-06-06 10:10:17 -0400

    Worked on task3

--- task3.py
+++ task3.py
@@ -2,3 +2,18 @@
 create a program  that takes in a 10 digit number, and outputs the isbn check digit
 
 '''
+def getChkDig(inStr):
+    sumRes = 0
+    for i in range (len(inStr)):
+        num = inStr[i]
+        num = int(num)
+        num = num*(11-i)
+        sumRes += num
+    remainder = sumRes % 11
+    if remainder == 0:
+        return (0)
+    answer = 11 - remainder
+    if answer == 10:
+        answer = 'X'
+
+    return (answer)
--- task3tests.py
+++ task3tests.py
@@ -1,10 +1,21 @@
-import task3
-import unittests
+import task3 as t3
+import unittest
 
 class TestSequenceFunctions(unittest.TestCase):
 
     def test1(self):
-        pass
-
+        assert t3.getChkDig('0000000001')==9 #test1 fails tenth digit fail
+        assert t3.getChkDig('0000000010')==8 #test1 Fails 9th digit check fail
+        assert t3.getChkDig('0000000100')==7 #test1 Fails 8th digit check fail
+        assert t3.getChkDig('0000001000')==6 #test1 Fails 7th digit check fail
+        assert t3.getChkDig('0000010000')==5 #test1 Fails 6th digit check fail
+        assert t3.getChkDig('0000100000')==4 #test1 Fails 5th digit check fail
+        assert t3.getChkDig('0001000000')==3 #test1 Fails 4th digit check fail
+        assert t3.getChkDig('0010000000')==2 #test1 Fails 3th digit check fail
+        assert t3.getChkDig('0100000000')==1 #test1 Fails 2th digit check fail
+        assert t3.getChkDig('1000000000')==0 #test1 Fails 1th digit check fail
+    
+    def test2(self):
+        assert t3.getChkDig('0000200000')=='X' #test2 Fails digit check x fail
 if __name__ == '__main__':
     unittest.main()
