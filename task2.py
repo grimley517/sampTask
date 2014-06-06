6d29eeb6f78a11dd9202693b4afedda0f2ab358f
Brian Jones <ba.jones@me.com>
2014-06-06 09:26:19 -0400

    added address book

--- task2.py
+++ task2.py
@@ -9,4 +9,14 @@ this program will be able to search the data file by surname to retrieve and dis
 
 by date of birth to retrieve and display all contacts with a birthday in a particular month.
 
+the data file organises data by comma seperated values.  
+
+surName, firstName, address1, address2, pCode, phone, dob, email
+
+
+breaking this into subtasks - the first task is to read the address file and put it into a data structure, 
+
+the second is to retrieve stuff from the data store and sort by name
+The third is to retrieve stuff by date of birth
 '''
+
--- task2tests.py
+++ task2tests.py
@@ -2,6 +2,9 @@ import task2
 import unittests
 
 class TestSequenceFunctions(unittest.TestCase):
+    
+    def setUp(self):
+        self.line1 = 'Jackson,Samantha,2 Heather Row,Basingstoke,RG21 3SD,01256 135434,23/04/1973,sam.jackson@hotmail.com'
 
     def test1(self):
         pass
